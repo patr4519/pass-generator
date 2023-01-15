08a73941be2b988835d1ec412696e5403808ccdd
patr4519 <jero96@mail.ru>
2023-01-15 22:12:05 +0300

    +

--- src/Functions/randomizer.js
+++ src/Functions/randomizer.js
@@ -1,4 +1,8 @@
 export function randomizer(array, lengthPass) {
+    if (array.length <= 10) {
+        array = [...array, ...array, ...array];
+    }
+
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
