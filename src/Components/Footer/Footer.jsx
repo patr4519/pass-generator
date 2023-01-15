50404a765f43e90485284758e022f04a24af405a
patr4519 <jero96@mail.ru>
2023-01-15 12:33:13 +0300

    +

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 export const Footer = () => {
     return (
         <footer>
-            <p>All rights reserved 2023</p>
+            <div>All rights reserved 2023</div>
         </footer>
     );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './Header.css'
-import key from '../../img/key.png';
+import key from '../../img/keyW.png';
 
 export const Header = () => {
     return (
